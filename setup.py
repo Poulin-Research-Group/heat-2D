4605febcaf9d7c92b220edbc1ef6d8ccf961f041
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2015-04-21 13:55:08 -0400

    code cleanup of sorts

--- setup.py
+++ setup.py
@@ -71,132 +71,125 @@ def serial_bdr(u, rank, px, py, col, row, tags, rankL, rankR, rankU, rankD):
     return u
 
 
-def set_x_bdr(u, rank, px, py, col, row, tags, rankL, rankR, rankU, rankD):
-    # Sends columns.
-    tagsL, tagsR, tagsU, tagsD = tags
-    col_block = rank % px
-
-    # Send odd-numbered columns
-    if col_block % 2:
-        comm.Send(u[:, 1].flatten(),  dest=rankL, tag=tagsL[rank])
-        comm.Send(u[:, -2].flatten(), dest=rankR, tag=tagsR[rank])
-
-    # Receive odd-numbered columns, send even-numbered columns
-    else:
-        comm.Recv(col, source=rankR, tag=tagsL[rankR])    # column from right
-        u[:, -1] = col
-        comm.Recv(col, source=rankL, tag=tagsR[rankL])    # column from left
-        u[:, 0]  = col
-
-        comm.Send(u[:, 1].flatten(),  dest=rankL, tag=tagsL[rank])
-        comm.Send(u[:, -2].flatten(), dest=rankR, tag=tagsR[rank])
-
-    # Receive even-numbered columns
-    if col_block % 2:
-        comm.Recv(col, source=rankR, tag=tagsL[rankR])    # column from right
-        u[:, -1] = col
-        comm.Recv(col, source=rankL, tag=tagsR[rankL])    # column from left
-        u[:, 0]  = col
-
-    return u
-
-
-def set_y_bdr(u, rank, px, py, col, row, tags, rankL, rankR, rankU, rankD):
-    # Sends rows
-    tagsL, tagsR, tagsU, tagsD = tags
-    row_block = rank // px
-
-    # Send odd-numbered rows
-    if row_block % 2:
-        comm.Send(u[ 1, :].flatten(), dest=rankU, tag=tagsU[rank])
-        comm.Send(u[-2, :].flatten(), dest=rankD, tag=tagsD[rank])
-
-    # Receive odd-numbered rows, send even-numbered rows
-    else:
-        comm.Recv(row, source=rankD, tag=tagsU[rankD])    # row from below
-        u[-1, :] = row
-        comm.Recv(row, source=rankU, tag=tagsD[rankU])    # row from above
-        u[0, :]  = row
-
-        comm.Send(u[ 1, :].flatten(), dest=rankU, tag=tagsU[rank])
-        comm.Send(u[-2, :].flatten(), dest=rankD, tag=tagsD[rank])
-
-    # Receive even-numbered rows
-    if row_block % 2:
-        comm.Recv(row, source=rankD, tag=tagsU[rankD])    # row from below
-        u[-1, :] = row
-        comm.Recv(row, source=rankU, tag=tagsD[rankU])    # row from above
-        u[0, :]  = row
-
-    return u
-
-
-def set_mpi_bdr2D(u, rank, px, py, col, row, tags, rankL, rankR, rankU, rankD):
-    # get location (row, col) of this rank's block
-    u = set_x_bdr(u, rank, px, py, col, row, tags, rankL, rankR, rankU, rankD)
-    u = set_y_bdr(u, rank, px, py, col, row, tags, rankL, rankR, rankU, rankD)
-
-    return u
-
-
-def writer(t_total, method, sc, opt=None):
-    if opt:
-        filename = './tests/%s/%s/sc-%d.txt' % (method, opt, sc)
-    else:
-        filename = './tests/%s/sc-%d.txt' % (method, sc)
-
-    # check to see if file exists; if it doesn't, create it.
-    if not os.path.exists(filename):
-        open(filename, 'a').close()
-
-    # write time to the file
-    F = open(filename, 'a')
-    F.write('%f\n' % t_total)
-    F.close()
-
-
-def get_ims_x(U, xg, yg, Nt):
-    ims = []
-    for j in xrange(Nt):
-        ims.append((plt.pcolormesh(xg[1:-1], yg[1:-1], U[:, :, j], norm=plt.Normalize(0, 1)), ))
-    return ims
-
-
-def get_ims_y(U, xg, yg, Nt, p):
-    ims = []
-    for j in xrange(Nt):
-        U_j = np.vstack([arr.transpose() for arr in np.array_split(U[:, :, j].transpose(), p)])
-        ims.append((plt.pcolormesh(xg[1:-1], yg[1:-1], U_j, norm=plt.Normalize(0, 1)), ))
-    return ims
-
-
-def get_ims_xy(U, xg, yg, nx, ny, Nt, p, px, py):
-    ims = []
-    for j in xrange(Nt):
-        U_j = U[:, :, j].reshape(ny, p*nx)
-        temp = [None for i in xrange(py)]
-        for i in xrange(py):
-            temp[i] = U_j[:, i*px*nx : (i+1)*px*nx]
-
-        U_j = np.vstack(temp)
-        ims.append((plt.pcolormesh(xg[1:-1], yg[1:-1], U_j, norm=plt.Normalize(0, 1)), ))
-    return ims
-
-
-def The_Animator(U, xg, yg, nx, ny, Nt, method, p, px, py):
-    fig = plt.figure()
-    print 'creating meshes...'
-    if px == 1:
-        ims = get_ims_y(U, xg, yg, Nt, p)
-    elif py == 1:
-        ims = get_ims_x(U, xg, yg, Nt)
-    else:
-        ims = get_ims_xy(U, xg, yg, nx, ny, Nt, p, px, py)
-    print 'done creating meshes, attempting to put them together...'
-    print 'saving...'
-    im_ani = animation.ArtistAnimation(fig, ims, interval=50, repeat_delay=3000, blit=False)
-    im_ani.save('./anims/MPI_SUPER_%s_%dpx_%dpy.mp4' % (method, px, py))
-    print 'saved.'
-
 METHODS   = ['numpy', 'f2py77', 'f2py90']
 UPDATERS  = [calc_u, heatf, heatf90]
+
+
+def solver_x(u, ranks, px, py, col, tags, params, save_solution):
+    """
+    Solves the 2D Heat Equation if it is parallelized only in x. If
+    save_solution is True, then solver_x_helper_g is called. Otherwise,
+    solver_x_helper is called.
+
+    Returns
+    -------
+    tuple (float64, ndarray)   if save_solution is true
+    tuple (float64, None)      if save_solution is false
+    """
+
+    if save_solution:
+        t_total, U = solver_x_helper_g(u, ranks, col, tags, params)
+    else:
+        t_total, U = solver_x_helper(u, ranks, col, tags, params)
+
+
+def solver_x_helper(u, ranks, col, tags, params):
+    p,  px, py = params.p_vars
+    C,  Kx, Ky = params.consts
+    Updater, Set_BCs = params.updater, params.bcs_func
+    Nt = params.Nt
+
+    rank, rankL, rankR, rankU, rankD = ranks
+    tagsL, tagsR, tagsU, tagsD = tags
+
+    comm.Barrier()         # start MPI timer
+    t_start = MPI.Wtime()
+
+    # loop through time
+    for j in range(1, Nt):
+        u = set_x_bdr(u, rank, px, col, tagsL, tagsR, rankL, rankR)
+        u = Updater(u, C, Kx, Ky)
+        u = Set_BCs(u, rank, p, px, py)
+
+    comm.Barrier()
+    t_total = (MPI.Wtime() - t_start)  # stop MPI timer
+    return t_total, None
+
+
+def solver_x_helper_g(u, ranks, col, tags, params):
+    p,  px, py = params.p_vars
+    C,  Kx, Ky = params.consts
+    nx, Ny, Nt = params.nx, params.ny, params.Nt
+    Updater, Set_BCs = params.updater, params.bcs_func
+
+    rank, rankL, rankR, rankU, rankD = ranks
+    tagsL, tagsR, tagsU, tagsD = tags
+    ug, U = create_global_vars(rank, params)
+
+    comm.Barrier()         # start MPI timer
+    t_start = MPI.Wtime()
+
+    # loop through time
+    for j in range(1, Nt):
+        u = set_x_bdr(u, rank, px, col, tagsL, tagsR, rankL, rankR)
+        u = Updater(u, C, Kx, Ky)
+        u = Set_BCs(u, rank, p, px, py)
+
+        # Gather parallel vectors to a serial vector
+        comm.Gather(u[1:-1, 1:-1].flatten(), ug, root=0)
+        if rank == 0:
+            U[:, :, j] = get_Uj(u, ug, p, nx, Ny)
+
+    comm.Barrier()
+    t_total = (MPI.Wtime() - t_start)  # stop MPI timer
+    return t_total, None
+
+
+def get_Uj(u, ug, p, nx, ny):
+    # evenly split ug into a list of p parts
+    temp = np.array_split(ug, p)
+    # reshape each part
+    temp = [a.reshape(ny, nx) for a in temp]
+    return np.hstack(temp)
+
+
+def create_global_vars(rank, params):
+    x0, xf, dx, Nx, nx = params.x_vars
+    y0, yf, dy, Ny, ny = params.y_vars
+    nx, ny, Nt = params.nx, params.ny, params.Nt
+    p = params.p
+    f = params.ics_func
+
+    if rank == 0:
+        xg = np.linspace(x0 - dx/2, xf + dx/2, Nx+2)
+        yg = np.linspace(y0 - dy/2, yf + dy/2, Ny+2)
+        ug = np.array([f(xg, j) for j in yg])[1:-1, 1:-1].flatten()
+        temp = np.array_split(ug, p)
+        temp = [a.reshape(ny, nx) for a in temp]
+
+        U = np.empty((ny, p*nx, Nt), dtype=np.float64)
+        U[:, :, 0] = np.hstack(temp)
+    else:
+        ug = None
+        U  = None
+
+    return ug, U
+
+
+class Params(object):
+    """Placeholder for several constants and what not."""
+    def __init__(self, x_vars, y_vars, t_vars, p_vars, consts, funcs):
+        super(Params, self).__init__()
+        self.x_vars = x_vars
+        self.y_vars = y_vars
+        self.t_vars = t_vars
+        self.p_vars = p_vars
+        self.consts = consts
+        self.funcs  = funcs
+
+        self.x0, self.xf, self.dx, self.Nx, self.nx = x_vars
+        self.y0, self.yf, self.dy, self.Ny, self.ny = y_vars
+        self.t0, self.tf, self.dt, self.Nt = t_vars
+        self.p,  self.px, self.py = p_vars
+        self.C,  self.Kx, self.Ky = consts
+        self.ics_func, self.bcs_func, self.updater = funcs
