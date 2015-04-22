fd76fc51dee2ff5741170f2cb5573ab578b7777f
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2015-04-22 13:37:04 -0400

    changed stepping_mpi_2D.py to make it easy to modify any params

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ import time
 from heatFortran import heatf
 from heatFortran90 import heatf as heatf90
 from mpi4py import MPI
+from fjp_helpers.animator import mesh_animator
+from fjp_helpers.bc import *
+from fjp_helpers.mpi import *
+from fjp_helpers.misc import *
 comm = MPI.COMM_WORLD
 
 
@@ -20,6 +24,94 @@ def calc_u(u, C, Kx, Ky):
 
 METHODS   = ['numpy', 'f2py77', 'f2py90']
 UPDATERS  = [calc_u, heatf, heatf90]
+
+
+def solver(Updater, x_vars, y_vars, t_vars, consts, f, sc_x, sc_y, px, py):
+
+    p = px * py
+    if p != comm.Get_size():
+        raise Exception("Incorrect number of cores used; MPI is being run with %d, but %d was inputted." % (comm.Get_size(), p))
+
+    rank  = comm.Get_rank()
+    indices = [(i, j) for i in xrange(py) for j in xrange(px)]
+    procs = np.arange(p).reshape(py, px)
+    locs  = dict(zip(procs.flatten(), indices))   # map rank to location
+    loc   = locs[rank]
+
+    left  = np.roll(procs,  1, 1)
+    right = np.roll(procs, -1, 1)
+    up    = np.roll(procs,  1, 0)
+    down  = np.roll(procs, -1, 0)
+    rankL = left[loc]
+    rankR = right[loc]
+    rankU = up[loc]
+    rankD = down[loc]
+
+    x0, xf, dx, Nx, nx = x_vars
+    y0, yf, dy, Ny, ny = y_vars
+    t0, tf, dt, Nt = t_vars
+    C, Kx, Ky = consts
+
+    x = create_x(px, rank, x0, xf, dx, nx, Nx)
+    y = create_y(px, py, rank, y0, yf, dy, ny, Ny)
+    t  = np.linspace(t0, tf, Nt)
+
+    # BUILD ZE GRID
+    xx, yy = np.meshgrid(x, y)
+    u   = f(xx, yy)
+    col = np.empty(ny+2, dtype='d')
+    row = np.empty(nx+2, dtype='d')
+
+    tagsL = dict([(j, j+1) for j in xrange(p)])
+    tagsR = dict([(j,   p + (j+1)) for j in xrange(p)])
+    tagsU = dict([(j, 2*p + (j+1)) for j in xrange(p)])
+    tagsD = dict([(j, 3*p + (j+1)) for j in xrange(p)])
+    tags  = (tagsL, tagsR, tagsU, tagsD)
+
+    SAVE_GLOBAL_SOLUTION = True
+    params = Params([x0, xf, dx, Nx, nx], [y0, yf, dy, Ny, ny], [t0, tf, dt, Nt],
+                    [p, px, py], [C, Kx, Ky], [0, 0, 0, 0])
+
+    if px == 1 and py == 1:
+        params.set_funcs([f, set_periodic_BC, Updater, None])
+        t_total, U = solver_serial(u, params, SAVE_GLOBAL_SOLUTION)
+
+    elif py == 1:
+        ranks = (rank,  rankL, rankR)
+        tags  = (tagsL, tagsR)
+        params.set_funcs([f, set_periodic_BC_y, Updater, send_cols_periodic])
+        t_total, U = solver_1D(u, ranks, col, tags, params, SAVE_GLOBAL_SOLUTION)
+
+    elif px == 1:
+        ranks = (rank,  rankU, rankD)
+        tags  = (tagsU, tagsD)
+        params.set_funcs([f, set_periodic_BC_x, Updater, send_rows_periodic])
+        t_total, U = solver_mpi_1D(u, ranks, row, tags, params, SAVE_GLOBAL_SOLUTION)
+
+    else:
+        ranks = (rank,  rankL, rankR, rankU, rankD)
+        tags  = (tagsL, tagsR, tagsU, tagsD)
+        params.set_funcs([f, None, Updater, send_periodic])
+        t_total, U = solver_mpi_2D(u, ranks, col, row, tags, params, SAVE_GLOBAL_SOLUTION)
+
+    # PLOTTING AND SAVING SOLUTION
+    if rank == 0:
+        if Updater is calc_u:
+            method = 'numpy'
+        elif Updater is heatf:
+            method = 'f2py77'
+        elif Updater is heatf90:
+            method = 'f2py90'
+
+        xg = np.linspace(x0 - dx/2, xf + dx/2, Nx+2)
+        yg = np.linspace(y0 - dy/2, yf + dy/2, Ny+2)
+
+        mesh_animator(U, xg, yg, nx, ny, Nt, method, p, px, py)
+
+        # writer(t_total, method, sc)
+        print t_total
+
+    return t_total
 
 
 def solver_serial(u, params, save_solution):
--- stepping_mpi_2D.py
+++ stepping_mpi_2D.py
@@ -1,7 +1,10 @@
 from __future__ import division
-from setup import np, comm, Params, sys, calc_u, solver_mpi_1D, solver_mpi_2D, \
-                  solver_serial
-from fjp_helpers.funcs import *
+import numpy as np
+import sys
+from setup import solver, comm, calc_u, heatf, heatf90, METHODS, UPDATERS
+from fjp_helpers.animator import mesh_animator
+from fjp_helpers.misc import writer
+
 
 # initial condition function
 def f(x, y):
@@ -9,119 +12,55 @@ def f(x, y):
     return np.sin(np.pi*x) * np.sin(np.pi*y)
 
 
-def main(Updater, sc=1, px=2, py=2):
-    # number of spatial points
-    Nx = 128*sc
-    Ny = 128*sc
+# number of processors to use in each direction
+px = 2
+py = 2
 
-    # allow user to input number of cores dedicated to x-axis and y-axis,
-    # then check to see if this is more than the actual number of cores
-    p = px * py
-    if p != comm.Get_size():
-        raise Exception("Incorrect number of cores used; MPI is being run with %d, but %d was inputted." % (comm.Get_size(), p))
+# scaling parameters
+sc_x = 1
+sc_y = 1
 
-    rank = comm.Get_rank()   # this process' ID
-    nx = Nx/px   # x-grid points per process
-    ny = Ny/py   # y-grid points per process
+# number of spatial points
+Nx = 128*sc_x
+Ny = 128*sc_y
 
-    indices = [(i, j) for i in xrange(py) for j in xrange(px)]
-    procs = np.arange(p).reshape(py, px)
-    locs  = dict(zip(procs.flatten(), indices))   # map rank to location
-    loc   = locs[rank]
+# number of (x, y) spatial points per processes dedicated to respective directions
+nx = Nx/px
+ny = Ny/py
 
-    left  = np.roll(procs,  1, 1)
-    right = np.roll(procs, -1, 1)
-    up    = np.roll(procs,  1, 0)
-    down  = np.roll(procs, -1, 0)
-    rankL = left[loc]
-    rankR = right[loc]
-    rankU = up[loc]
-    rankD = down[loc]
+# x conditions
+x0 = 0                  # start
+xf = 1                  # end
+dx = (xf-x0)/(Nx+1)     # spatial step size
 
-    # x conditions
-    x0 = 0                  # start
-    xf = 1                  # end
-    dx = (xf-x0)/(Nx+1)     # spatial step size
-    # this takes the interval [x0,xf] and splits it equally among all processes
-    x = create_x(px, rank, x0, xf, dx, nx, Nx)
+# y conditions
+y0 = 0
+yf = 1
+dy = (yf-y0)/(Ny+1)
 
-    # y conditions
-    y0 = 0
-    yf = 1
-    dy = (yf-y0)/(Ny+1)
-    y = create_y(px, py, rank, y0, yf, dy, ny, Ny)
+# temporal conditions
+Nt = 1000          # number of time steps
+t0 = 0
+tf = 300
+dt = (tf - t0)/Nt
 
-    # temporal conditions
-    Nt  = 1000         # time steps
-    t0 = 0             # start
-    tf = 300           # end
-    dt = (tf - t0)/Nt  # time step size
-    t  = np.linspace(t0, tf, Nt)
+# PDE coefficients
+Kx = 0.02
+Ky = 0.01
+C  = 1 - 2*(Kx + Ky)
 
-    # coefficients
-    k  = 0.0002
-    Kx = 0.02                # PDE coeff for x terms
-    Ky = 0.01
-    C  = 1 - 2*(Kx + Ky)
+# Method to use (numpy, f2py-f77, f2py-f90 ...)
+Updater = calc_u
 
-    # BUILD ZE GRID
-    xx, yy = np.meshgrid(x, y)
-    u   = f(xx, yy)
-    col = np.empty(ny+2, dtype='d')
-    row = np.empty(nx+2, dtype='d')
-
-    tagsL = dict([(j, j+1) for j in xrange(p)])
-    tagsR = dict([(j,   p + (j+1)) for j in xrange(p)])
-    tagsU = dict([(j, 2*p + (j+1)) for j in xrange(p)])
-    tagsD = dict([(j, 3*p + (j+1)) for j in xrange(p)])
-    tags  = (tagsL, tagsR, tagsU, tagsD)
-
-    SAVE_GLOBAL_SOLUTION = True
-    params = Params([x0, xf, dx, Nx, nx], [y0, yf, dy, Ny, ny], [t0, tf, dt, Nt],
-                    [p, px, py], [C, Kx, Ky], [0, 0, 0, 0])
-
-    if px == 1 and py == 1:
-        params.set_funcs([f, set_periodic_BC, Updater, None])
-        t_total, U = solver_serial(u, params, SAVE_GLOBAL_SOLUTION)
-
-    elif py == 1:
-        ranks = (rank,  rankL, rankR)
-        tags  = (tagsL, tagsR)
-        params.set_funcs([f, set_periodic_BC_y, Updater, send_columns_periodic])
-        t_total, U = solver_1D(u, ranks, col, tags, params, SAVE_GLOBAL_SOLUTION)
-
-    elif px == 1:
-        ranks = (rank,  rankU, rankD)
-        tags  = (tagsU, tagsD)
-        params.set_funcs([f, set_periodic_BC_x, Updater, send_rows_periodic])
-        t_total, U = solver_mpi_1D(u, ranks, row, tags, params, SAVE_GLOBAL_SOLUTION)
-
-    else:
-        ranks = (rank,  rankL, rankR, rankU, rankD)
-        tags  = (tagsL, tagsR, tagsU, tagsD)
-        params.set_funcs([f, None, Updater, send_periodic])
-        t_total, U = solver_mpi_2D(u, ranks, col, row, tags, params, SAVE_GLOBAL_SOLUTION)
-
-    # PLOTTING AND SAVING SOLUTION
-    if rank == 0:
-        if Updater is calc_u:
-            method = 'numpy'
-        elif Updater is heatf:
-            method = 'f2py77'
-        elif Updater is heatf90:
-            method = 'f2py90'
-
-        xg = np.linspace(x0 - dx/2, xf + dx/2, Nx+2)
-        yg = np.linspace(y0 - dy/2, yf + dy/2, Ny+2)
-
-        mesh_animator(U, xg, yg, nx, ny, Nt, method, p, px, py)
-
-        writer(t_total, method, sc)
-        print t_total
-
-    return t_total
+# DO NOT ALTER =======================================
+x_vars = [x0, xf, dx, Nx, nx]
+y_vars = [y0, yf, dy, Ny, ny]
+t_vars = [t0, tf, dt, Nt]
+consts = [C, Kx, Ky]
 
 
+"""
+# handling command line arguments
 if len(sys.argv) > 1:
     get_updater = dict(zip(METHODS, UPDATERS))
 
@@ -131,8 +70,9 @@ if len(sys.argv) > 1:
     px = int(argv[2])
     py = int(argv[3])
 
-    main(updater, sc, px, py)
+    (Updater, sc, px, py)
+    comm.Barrier()
+    sys.exit()
+"""
 
-else:
-    #    method, sc, px, py
-    main(calc_u, 1, 1, 1)
+solver(Updater, x_vars, y_vars, t_vars, consts, f, sc_x, sc_y, px, py)
